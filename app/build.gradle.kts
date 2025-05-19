68692c26595f30b1200152a194977c7e7215635e
serge <sergejrusanov03001@gmail.com>
2025-05-19 15:10:05 +0300

    main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3"
+        versionName = "1.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
